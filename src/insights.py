c725424c49e42cb0f8c9a6d467570e0383dbc9ab
joaopaulo-ern <joaopaulo.ern@gmail.com>
2022-10-23 12:58:47 -0300

    requisito2

--- src/insights.py
+++ src/insights.py
@@ -1,4 +1,16 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
+    data = read(path)
+    conjunto = set()
+
+    for linha in data:
+        if linha['job_type'] != '':
+            conjunto.add(linha['job_type'])
+
+    return conjunto
+
     """Checks all different job types and returns a list of them
 
     Must call `read`
@@ -13,7 +25,6 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
 
 
 def filter_by_job_type(jobs, job_type):
