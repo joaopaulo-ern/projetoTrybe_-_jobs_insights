ac9177ab37fe5bea1bf9c7ebdcd2dd62461bcfa8
joaopaulo-ern <joaopaulo.ern@gmail.com>
2022-10-23 23:08:13 -0300

    requisito10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
+    javascript = count_ocurrences('src/jobs.csv', 'javascript')
+    PYTHON = count_ocurrences('src/jobs.csv', 'PYTHON')
+    assert javascript == 122
+    assert PYTHON == 1639
     pass
