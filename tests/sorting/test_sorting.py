2952a8e83f700d72f73eab38c1cfa1c82566f775
joaopaulo-ern <joaopaulo.ern@gmail.com>
2022-10-25 17:20:42 -0300

    requisito12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,21 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+from operator import itemgetter
+
+jobs = [
+    {'min_salary': 2500, 'max_salary': 3500, 'date_posted': '1995-01-02'},
+    {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2005-01-02'},
+    {'min_salary': 1500, 'max_salary': 2500, 'date_posted': '2015-01-02'},
+    {'min_salary': 2000, 'max_salary': 3000, 'date_posted': '2020-01-02'},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, 'min_salary')
+    assert jobs == sorted(jobs, key=itemgetter('min_salary'))
+
+    sort_by(jobs, 'max_salary')
+    assert jobs == sorted(jobs, key=itemgetter('max_salary'), reverse=True)
+
+    sort_by(jobs, 'date_posted')
+    assert jobs == sorted(jobs, key=itemgetter('date_posted'), reverse=True)
