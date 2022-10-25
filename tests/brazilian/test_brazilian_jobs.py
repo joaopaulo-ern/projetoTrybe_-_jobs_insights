c2b5b841854191246e44bd4d342472362380568e
joaopaulo-ern <joaopaulo.ern@gmail.com>
2022-10-25 13:47:42 -0300

    requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for object in mock:
+        assert 'title' and 'salary' and 'type' in object
+        assert 'titulo' and 'salario' and 'tipo' not in object
