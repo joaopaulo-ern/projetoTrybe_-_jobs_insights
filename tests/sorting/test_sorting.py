10ea946ca08fb5bdefd613acb4af7da34e6fca61
joaopaulo-ern <joaopaulo.ern@gmail.com>
2022-10-26 09:33:05 -0300

    requisito13

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,20 +2,22 @@ from src.sorting import sort_by
 
 from operator import itemgetter
 
+
 jobs = [
     {'min_salary': 2500, 'max_salary': 3500, 'date_posted': '1995-01-02'},
-    {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2005-01-02'},
-    {'min_salary': 1500, 'max_salary': 2500, 'date_posted': '2015-01-02'},
-    {'min_salary': 2000, 'max_salary': 3000, 'date_posted': '2020-01-02'},
+    {'min_salary': 1000, 'max_salary': 2000, 'date_posted': '2000-01-02'},
+    {'min_salary': 1500, 'max_salary': 2500, 'date_posted': '2005-01-02'},
+    {'min_salary': 2000, 'max_salary': 3000, 'date_posted': '2010-01-02'},
 ]
 
 
 def test_sort_by_criteria():
-    sort_by(jobs, 'min_salary')
-    assert jobs == sorted(jobs, key=itemgetter('min_salary'))
 
     sort_by(jobs, 'max_salary')
     assert jobs == sorted(jobs, key=itemgetter('max_salary'), reverse=True)
+
+    sort_by(jobs, 'min_salary')
+    assert jobs == sorted(jobs, key=itemgetter('min_salary'))
 
     sort_by(jobs, 'date_posted')
     assert jobs == sorted(jobs, key=itemgetter('date_posted'), reverse=True)
